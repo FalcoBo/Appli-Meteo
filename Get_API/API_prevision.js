06b7a723ed57b3a034e986602cd7e28e9ea3f272
Theo <118967935+FalcoBo@users.noreply.github.com>
2023-03-20 10:50:38 +0100

    fix

--- Get_API/API_prevision.js
+++ Get_API/API_prevision.js
@@ -1,19 +1,21 @@
 // Function qui permet de récupérer une autre API pour pouvoir afficher
 // les prévisions météo
 
-function API_prevision() {
-    fetch("")
-    .then(Response => Response.json())
-    .then(data => {
-        // Key 
-        var request_key = data.cod;
-        if (request_key === 200 ) {
-            // Récupération des données de l'API des prévisions
+// https://www.infoclimat.fr/public-api/gfs/json?_ll=48.85341,2.3488&_auth=VE4DFAB%2BVXdfclBnVCJSe1c%2FUmdeKAUiAHwEZw9qXyICaVc2UzNXMVE%2FA34FKgQyUXwCYVtgBDQFbgpyAHJeP1Q%2BA28Aa1UyXzBQNVR7UnlXeVIzXn4FIgBrBGMPfF87AmFXLVMzVzJRIANmBTEEL1F9AmNbYgQ7BW8KbwBkXjRUMgNjAGBVKF8vUDRUYFJnV2xSM15iBWkAZAQwD2tfOwJlVzFTM1crUT0DZAUzBDRRZgJqW2IEPgV5CnIAFF5OVCoDJwAhVWJfdlAvVDFSOFcw&_c=ca4123a50427d66be36a88f6e0ee0e7d
+
+// function API_prevision() {
+//     fetch("")
+//     .then(Response => Response.json())
+//     .then(data => {
+//         // Key 
+//         var request_key = data.cod;
+//         if (request_key === 200 ) {
+//             // Récupération des données de l'API des prévisions
 
 
-        } else {
-            console.log("La récupération de l'API n'a pas été effectuée")
-        }
-    })
-    .catch(error => console.log(error));
-}
+//         } else {
+//             console.log("La récupération de l'API n'a pas été effectuée")
+//         }
+//     })
+//     .catch(error => console.log(error));
+// }
